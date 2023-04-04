11d844fc95e34d0fa5a92f5b2938dab89982e081
WilliamWu-MS <93303494+WilliamWu-MS@users.noreply.github.com>
2023-04-04 15:58:51 +0800

    Update IntelligentCamera.js

--- IntelligentCamera.js
+++ IntelligentCamera.js
@@ -1,70 +1,75 @@
 const Form = JSONSchemaForm.default;
 const schema = {
-  "type":"object",
-    "title":"Intelligent Camera Certification Form",
-    "properties":{
-        "physicalCameras":{
-            "type":"array",
-            "title":"Physical Cameras",
-            "items":{
-                "type":"object",
-                "properties":{
-                    "name":{
-                        "type":"string",
-                        "title":"Camera Name",
-                        "enum":["camera 1","camera 2","camera 3","camera 4","camera 5","camera 6","camera 7"]
-                    },
-                    "FoV":{
-                        "type":"number",
-                        "title":"FoV(in degree)"
-                    },
-                    "Center":{
-                        "type":"number",
-                        "title":"Center Position(in degree)"
-                    }
-                }
-            }
-        },
-        "logicalCameras":{
-            "type":"array",
-            "title":"Logcial Cameras",
-            "items":{
-                "type":"object",
-                "properties":{
-                    "name":{
-                        "type":"string",
-                        "title":"Camera Name"
-                    },
-                    "cameraType":{
-                        "type":"string",
-                        "title":"Type",
-                        "enum":[
-                            "panoramic",
-                            "people feed",
-                            "composite feed",
-                            "content",
-                            "test 1",
-                            "test 2",
-                            "test 3"
-                        ]
-                    },
-                    "FoV":{
-                        "type":"number",
-                        "title":"FoV"
-                    },
-                    "physicalCamera":{
-                        "type":"array",
-                        "title":"Physical Cameras",
-                        "items":{
-                            "type":"string",
-                            "title":"name",
-                            "enum":["camera 1","camera 2","camera 3","camera 4","camera 5","camera 6","camera 7"]
-                        }
-                    }
-                }
-            }
+  "type": "object",
+  "title": "Intelligent Camera Certification Form",
+  "properties": {
+    "physicalCameras": {
+      "type": "array",
+      "title": "Physical Cameras",
+      "items": {
+        "type": "object",
+        "properties": {
+          "name": {
+            "type": "string",
+            "title": "Camera Name",
+            "enum": [
+              "camera 1",
+              "camera 2",
+              "camera 3",
+              "camera 4",
+              "camera 5",
+              "camera 6",
+              "camera 7"
+            ]
+          },
+          "FoV": {
+            "type": "number",
+            "title": "FoV(in degree)"
+          },
+          "Center": {
+            "type": "number",
+            "title": "Center Position(in degree)"
+          }
         }
+      }
+    },
+    "logicalCameras": {
+      "type": "object",
+      "title": "Logcial Cameras",
+      "properties": {
+        "numPeopleFeed": {
+          "type": "number",
+          "title": "number of people feed"
+        },
+        "peopleFeedType": {
+          "type": "string",
+          "title": "People Feed Type",
+          "enum": [
+            "single source",
+            "overlap",
+            "stithcing"
+          ]
+        },
+        "physicalCamera": {
+          "type": "array",
+          "title": "Physical Cameras",
+          "items": {
+            "type": "string",
+            "title": "name",
+            "enum": [
+              "camera 1",
+              "camera 2",
+              "camera 3",
+              "camera 4",
+              "camera 5",
+              "camera 6",
+              "camera 7"
+            ]
+          }
+        }
+      }
     }
+  }
 };
 
 const onSubmit = ({ formData }, e) => console.log("Data submitted: ", formData);
